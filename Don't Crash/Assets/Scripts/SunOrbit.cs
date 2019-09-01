520eeb0e95c87d6993997858a58e5b6496ffba7e
KaneTesta <42565992+KaneTesta@users.noreply.github.com>
2019-09-02 00:07:41 +1000

    scene done
    Need to add shading

---
+++ Don't Crash/Assets/Scripts/SunOrbit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunOrbit : MonoBehaviour
+{
+
+    private float orbitRadius = 20.0f; 
+    public float rotateSpeed = 0.1f;
+    private float timeCount = 0;
+    public bool nightMode = false;
+
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(orbitRadius,0.0f,0.0f);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCount += (Time.deltaTime*rotateSpeed);
+        float x = Mathf.Cos(timeCount)*orbitRadius;
+        float y = Mathf.Sin(timeCount)*orbitRadius;
+
+        if (y>0 && nightMode == true){
+            nightMode = false;
+        } else if (y<0 && nightMode == false){
+            nightMode = true;
+        }
+        transform.position = new Vector3(x,y,0.0f);
+    }
+
+    public Vector3 GetPosition() {
+        return this.transform.position;
+    }
+}
