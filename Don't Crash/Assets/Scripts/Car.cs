30e4f330c774ab8d6c8864e706cb323dfb7a0362
Kane <42565992+KaneTesta@users.noreply.github.com>
2019-09-04 00:34:08 +1000

    added cars

---
+++ Don't Crash/Assets/Scripts/Car.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    //Car attributes that are altered as time goes on
+    public float speedMultiplier = 1.0f;
+
+    // Global car attributes
+    private int speed = 1;
+    private bool cantStop = false; // for cars that never can be stopped so player must prioritise getting them through
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = this.transform.position;
+        newPos.z += speed*speedMultiplier*Time.deltaTime;
+        this.transform.position = newPos;
+    }
+}
---
+++ Don't Crash/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControl : MonoBehaviour
+{
+    //Initial spawn locations of cars {Bot Left x2, Top Left x2, Top Right x2, BotRight x2 }
+    private float[] startingX = new float[] {-0.4f, -1.2f, -9.9f, -9.9f, 0.4f, 1.2f, 9.9f, 9.9f};
+    private float[] startingZ = new float[] {-11.0f, -11.0f, 0.4f, 1.27f, 11.0f, 11.0f, -1.27f, -0.4f};
+    private float[] rotationY = new float[] {0f,0f,90f,90f,180f,180f,270f,270f};
+    private float startingY = 0.35f;
+
+    public float spawnIntervalMax = 0.1f;
+    public float spawnIntervalMin = 2.0f;
+
+
+    //Car Prefabs
+    public GameObject defaultCar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnCar", spawnIntervalMax, spawnIntervalMax);
+    }
+
+    void SpawnCar()
+    {
+        // CONFIGURE -- Random spawning point
+        int rand = UnityEngine.Random.Range(0,8);
+
+        //CONFIGURE -- Random car type
+        int randomCarType = 0;
+
+        Quaternion direction = Quaternion.identity;
+        direction.eulerAngles = new Vector3(0,rotationY[rand],0);
+
+        GameObject newCar = Instantiate(defaultCar, new Vector3(startingX[rand], startingY, startingZ[rand]),direction);
+    }
+}
