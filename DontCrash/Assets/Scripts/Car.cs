9eecd8ff47003f3a534a8679ffcb9d0ea784502d
KaneTesta <42565992+KaneTesta@users.noreply.github.com>
2019-09-04 23:07:04 +1000

    Downgraded to Unity 2019.18

---
+++ DontCrash/Assets/Scripts/Car.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    //Level Controller
+    private GameObject controller;
+
+    //Car attributes that are altered as time goes on
+    public float speedMultiplier = 1.0f;
+
+    //Map attributes
+    private int mapWidth = 20;
+
+    // Global car attributes
+    private int speed = 5;
+    private bool cantStop = false; // for cars that never can be stopped so player must prioritise getting them through
+    private int zCoEff;
+    private int xCoEff;
+    private float initX;
+    private float initZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.Find("LevelController");
+        zCoEff = zDirection(this.transform.eulerAngles);
+        xCoEff = xDirection(this.transform.eulerAngles);
+        initX = this.transform.position.x;
+        initZ = this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = this.transform.position;
+        newPos.x = newPos.x + xCoEff*speed*speedMultiplier*Time.deltaTime;
+        newPos.z = newPos.z + zCoEff*speed*speedMultiplier*Time.deltaTime;
+        this.transform.position = newPos;
+
+        killCheck();
+    }
+
+    int zDirection(Vector3 rot){
+        int coEff = 0;
+        if (rot.y == 180f) {
+            coEff = -1;
+        } else if (rot.y == 0f) {
+            coEff = 1;
+        } 
+        return coEff;
+    }
+
+    int xDirection(Vector3 rot){
+        int coEff = 0;
+        if (rot.y == 90f) {
+            coEff = 1;
+        } else if (rot.y == 270f) {
+            coEff = -1;
+        }
+        return coEff;    
+    }
+
+    //Kill car when out of bounds and add point
+    void killCheck(){
+        //ADD GLOBAL POINT ADDITION TO LEVEL CONTROLLER
+        
+        if (xCoEff == 1){
+            if (this.transform.position.x > initX + mapWidth){
+                Destroy(this.gameObject);
+                addPoint();
+            }
+        } else if (xCoEff == -1){
+            if (this.transform.position.x < initX - mapWidth){
+                Destroy(this.gameObject);
+                addPoint();
+            }
+        } else if (zCoEff == 1){
+            if (this.transform.position.z > initZ + mapWidth){
+                Destroy(this.gameObject);
+                addPoint();
+            }
+        } else if (zCoEff == -1){
+            if (this.transform.position.z < initZ - mapWidth){
+                Destroy(this.gameObject);
+                addPoint();
+            }
+        }
+    }
+
+    void addPoint(){
+        controller.GetComponent<LevelControl>().score += 1;
+    }
+
+}
---
+++ DontCrash/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControl : MonoBehaviour
+{
+    //Initial spawn locations of cars {Bot Left x2, Top Left x2, Top Right x2, BotRight x2 }
+    private float[] startingX = new float[] {-0.4f, -1.2f, -9.9f, -9.9f, 0.4f, 1.2f, 9.9f, 9.9f};
+    private float[] startingZ = new float[] {-11.0f, -11.0f, 0.4f, 1.27f, 11.0f, 11.0f, -1.27f, -0.4f};
+    private float[] rotationY = new float[] {0f,0f,90f,90f,180f,180f,270f,270f};
+    private float startingY = 0.229f;
+
+    private float spawnIntervalMax = 3.0f;
+    private float spawnIntervalMin = 1.0f;
+
+
+    //Car Prefabs
+    public GameObject defaultCar;
+    private bool readyToSpawn = true;
+    private Color[] carColor = new Color[] {new Color(0.02f,0.93f,1.0f,0.2f),new Color(0.14f,0.83f,0.19f,0.2f),new Color(0.78f,0.36f,0.34f,0.2f)};
+
+
+    //Game controls
+    public int score = 0;
+
+
+    void Update()
+    {
+        if (readyToSpawn)
+        {
+            float timer = Random.Range(spawnIntervalMin, spawnIntervalMax);
+            Invoke("SpawnCar", timer);
+            readyToSpawn = false;
+        }
+    }
+
+    void SpawnCar()
+    {
+        // CONFIGURE -- Random spawning point
+        int rand = UnityEngine.Random.Range(0,8);
+
+        //CONFIGURE -- Random car type
+        int randomCarType = 0;       
+
+        Quaternion direction = Quaternion.identity;
+        direction.eulerAngles = new Vector3(0,rotationY[rand],0);
+
+        GameObject newCar = Instantiate(defaultCar, new Vector3(startingX[rand], startingY, startingZ[rand]),direction);
+       
+       // Change color of body of default car
+        if (randomCarType == 0){
+            int randomCarColor = Random.Range(0,3);
+            foreach (Transform child in newCar.transform){
+
+                if (child.name == "Top" || child.name == "Bottom"){
+                    child.GetComponent<Renderer>().material.color = carColor[randomCarColor];
+                }
+            }
+        }
+       
+        readyToSpawn = true;
+
+    }
+}
