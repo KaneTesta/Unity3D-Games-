534575ad15ee0c8db86ea5903b48b3a7a2441a88
KaneTesta <42565992+KaneTesta@users.noreply.github.com>
2019-09-16 00:21:03 +1000

    UI Basics Done

---
+++ DontCrash/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu: MonoBehaviour
+{
+    public GameObject MenuUI;
+    public GameObject ControlsUI;
+
+    private Color red = Color.red;
+    private Color blue = new Color(0.03f,0.6f,1f,1f);
+    private Color white = Color.white;
+    private int upTo = 0;
+    public TMPro.TextMeshProUGUI bg;
+
+    public void Start(){
+        float timer = 0.15f;
+        InvokeRepeating("ColorChange", timer, timer);
+    }
+
+    //Change Colour Gradient to resemble police sirens
+    void ColorChange(){
+        Color[] colors = new Color[] {red, white, blue, white};
+        bg.color = colors[upTo];
+        upTo += 1;
+
+        if (upTo == 4){
+            upTo = 0;
+        }
+
+    }
+
+    public void Controls(){
+        MenuUI.SetActive(false);
+        ControlsUI.SetActive(true);
+    }
+
+
+    public void StartGame(){
+        MenuUI.SetActive(false);
+        ControlsUI.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ShowMenu(){
+        Time.timeScale = 1f;
+        MenuUI.SetActive(true);
+        ControlsUI.SetActive(false);
+    }
+}
---
+++ DontCrash/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)){
+            if (gameIsPaused){
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void Restart(){
+        Resume();
+        SceneManager.LoadScene("Main");
+    }
+
+
+    public void Exit(){
+        pauseMenuUI.SetActive(false);
+        gameIsPaused = false;
+        SceneManager.LoadScene("Main");
+        this.GetComponent<MainMenu>().ShowMenu();
+    }
+}
